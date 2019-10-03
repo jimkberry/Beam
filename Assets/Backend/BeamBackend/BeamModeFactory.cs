6dfce635d472b71f200a289f83b952eab48ac745
jim berry <jim@bigfattail.com>
2019-10-03 10:45:24 -0400

    Add ModeSPlash to backend (empty)

--- Assets/Backend/BeamBackend/BeamModeFactory.cs
+++ Assets/Backend/BeamBackend/BeamModeFactory.cs
@@ -7,12 +7,14 @@ namespace BeamBackend
     public class BeamModeFactory : ModeFactory
     {
         public static int kStartup = 0;
-        public const int kPlay = 1;
+        public const int kSplash = 1;        
+        public const int kPlay = 2;
 
         public BeamModeFactory()
         {      
             modeFactories =  new Dictionary<int, Func<IGameMode>>  {  
                 { kStartup, ()=> new ModeStartup() },  
+                { kSplash, ()=> new ModeSplash() },                 
                 { kPlay, ()=> new ModePlay() },                      
             }; 
         }       
