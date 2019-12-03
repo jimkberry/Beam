6171d09696c1067098377a4a3203f8d6386c58b9
jim berry <jim@bigfattail.com>
2019-12-03 16:50:41 -0500

    User settings panel

---
+++ Assets/Scripts/UI/SettingsBtn.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsBtn : UIBtn
+{
+    public GameObject target;
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+		base.Start();	        
+    }
+
+    // Update is called once per frame
+    protected override void Update()
+    {
+		base.Update();	        
+    }
+
+	public override void doSelect()
+	{
+        Debug.Log(string.Format("FrontendBike.Start()"));          
+		target.transform.GetComponent<MovableUISetItem>().toggle();
+	}    
+}
