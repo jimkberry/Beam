9d3d7330804c8320722c9afe10ed99ed5eb08b6d
jim berry <jim@bigfattail.com>
2020-01-22 12:35:18 -0500

    Added GameID field to allow joiing

--- Assets/Scripts/SettingsPanel.cs
+++ Assets/Scripts/SettingsPanel.cs
@@ -11,7 +11,7 @@ public class SettingsPanel : MovableUISetItem
     public GameObject p2pConnectionField;
     public GameObject ethNodeField;
     public GameObject ethAcctField;
- 
+     public GameObject gameIdField;
 
     public void LoadAndShow()
     {
@@ -22,6 +22,8 @@ public class SettingsPanel : MovableUISetItem
         p2pConnectionField.GetComponent<InputField>().text = settings.p2pConnectionString;
         ethNodeField.GetComponent<InputField>().text = settings.ethNodeUrl;
         ethAcctField.GetComponent<InputField>().text = settings.ethAcct;
+        gameIdField.GetComponent<InputField>().text = 
+            settings.tempSettings.ContainsKey("gameId") ? settings.tempSettings["gameId"] : "";
 
         base.moveOnScreen();
     }
@@ -35,6 +37,9 @@ public class SettingsPanel : MovableUISetItem
         settings.p2pConnectionString = p2pConnectionField.GetComponent<InputField>().text;
         settings.ethNodeUrl = ethNodeField.GetComponent<InputField>().text;
         settings.ethAcct = ethAcctField.GetComponent<InputField>().text;
+        string gameId = gameIdField.GetComponent<InputField>().text;
+        if (gameId != "")
+            settings.tempSettings["gameId"] = gameId;
 
         base.moveOffScreen();
     }
